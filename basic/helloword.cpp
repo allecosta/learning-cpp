e987080f28ea3f8521624f8058a513725c9e6ade
allecosta <alle_costa10@outlook.com>
2022-10-29 17:41:00 -0300

    hello word

---
+++ basic/helloword.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Olá Mundo C++ !!!" << endl;
+    cout << "Aprofudando os meus estudos em segurança da informação";
+
+    return 0;
+}
